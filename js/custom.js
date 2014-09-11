22ac7efef913b23055f0152843312f61846ac557
dronni3de <example@email.com>
2014-09-11 19:46:37 +0400

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+  $(document).ready(function(){  
+    
+    // $(".btn-click-submit").click(function(){
+    //     $(".form-submit .btn").click();
+    //     $('html,body').animate({scrollTop: $('.form-submit').offset().top}, 1000);
+    //     return false;
+    // });
+
+    $(".bgd1").anystretch('img/bgd-1.jpg'); 
+    $(".bgd2").anystretch('img/bgd-2.jpg'); 
+    $(".bgd3").anystretch('img/bgd-3.jpg'); 
+    $(".bgd4").anystretch('img/bgd-4.jpg'); 
+    $(".bgd5").anystretch('img/bgd-5.jpg'); 
+    $(".bgd6").anystretch('img/bgd-6.jpg'); 
+    $(".bgd7").anystretch('img/bgd-7.jpg'); 
+    $(".bgd8").anystretch('img/bgd-8.jpg'); 
+    $(".bgd9").anystretch('img/bgd-9.jpg'); 
+  
+  });
